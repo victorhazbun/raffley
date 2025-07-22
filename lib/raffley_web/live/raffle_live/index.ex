d47a01dd5cbd96056e7105f0d54ec2196bb97763
Victor Hazbun <victorhazbun87@gmail.com>
2025-07-22 18:46:05 +0000

    Live Navigation

--- lib/raffley_web/live/raffle_live/index.ex
+++ lib/raffley_web/live/raffle_live/index.ex
@@ -32,16 +32,18 @@ defmodule RaffleyWeb.RaffleLive.Index do
 
   def raffle_card(assigns) do
     ~H"""
-    <div class="card">
-      <img src={@raffle.image_path} />
-      <h2>{@raffle.prize}</h2>
-      <div class="details">
-        <div class="price">
-          ${@raffle.ticket_price} / ticket
+    <.link navigate={~p"/raffles/#{@raffle}"}>
+      <div class="card">
+        <img src={@raffle.image_path} />
+        <h2>{@raffle.prize}</h2>
+        <div class="details">
+          <div class="price">
+            ${@raffle.ticket_price} / ticket
+          </div>
+          <.badge status={@raffle.status} />
         </div>
-        <.badge status={@raffle.status} />
       </div>
-    </div>
+    </.link>
     """
   end
 end
