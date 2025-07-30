1244fcb6375d6917e48d98dd73568d20d81e05fc
Victor Hazbun <victorhazbun87@gmail.com>
2025-07-30 01:43:08 +0000

    Generate the Auth System

--- config/dev.exs
+++ config/dev.exs
@@ -6,6 +6,7 @@ config :raffley, Raffley.Repo,
   password: "postgres",
   hostname: "localhost",
   database: "raffley_dev",
+  port: "5433",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
